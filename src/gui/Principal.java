03db3f8db04ab89f85843c466ba37c49ac056b3f
USER <USER@Aza-ky>
2024-11-02 17:46:18 -0500

    ordenando los archivos en respectivas carpetas

--- src/gui/Principal.java
+++ src/gui/Principal.java
@@ -1,9 +1,11 @@
+package gui;
+
 import dao.DAOProducto;
 import dto.DTOProducto;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Principal {
     public static void main(String[] args) {
         DAOProducto daoProducto = new DAOProducto();
         Scanner sc = new Scanner(System.in);
